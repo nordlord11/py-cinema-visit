729bb154ad40ae608102dceb148a4a8957ab79d7
Vadym Polishchuk <nemezis.vp@gmail.com>
2026-08-20 13:48:02 +0300

    Fix hall.py customers: list[Customer]

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,5 +1,5 @@
 from app.people.cinema_staff import Cleaner
-
+from app.people.customer import Customer
 
 class CinemaHall:
 
@@ -9,7 +9,7 @@ class CinemaHall:
     def movie_session(
         self,
         movie_name: str,
-        customers: list,
+        customers: list[Customer],
         cleaning_staff: Cleaner
     ) -> None:
 
